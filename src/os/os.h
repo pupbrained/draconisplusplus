11096c5ec77afd16acaa5d12a43105775d224ad1
pupbrained <mars@pupbrained.xyz>
2024-06-16 02:39:02 -0400

    a lil more documentatin

--- src/os/os.h
+++ src/os/os.h
@@ -5,6 +5,17 @@
 #include "util/macros.h"
 #include "util/numtypes.h"
 
+/**
+ * @brief Get the amount of installed RAM in bytes.
+ */
 fn GetMemInfo() -> u64;
+
+/**
+ * @brief Get the currently playing song metadata.
+ */
 fn GetNowPlaying() -> std::string;
+
+/**
+ * @brief Get the OS version.
+ */
 fn GetOSVersion() -> const char*;
