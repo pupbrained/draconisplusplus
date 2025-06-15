9a6b9bbefa975e79e75c89d71020fc4e8013a700
Mars <mars@pupbrained.xyz>
2025-06-15 15:43:43 -0400

    o

--- include/DracUtils/Types.hpp
+++ include/DracUtils/Types.hpp
@@ -1,16 +1,16 @@
 #pragma once
 
-#include <array>       // std::array (Array)
-#include <future>      // std::future (Future)
-#include <map>         // std::map (Map)
-#include <memory>      // std::shared_ptr and std::unique_ptr (SharedPointer, UniquePointer)
-#include <mutex>       // std::mutex and std::lock_guard (Mutex, LockGuard)
-#include <optional>    // std::optional (Option)
-#include <string>      // std::string (String, StringView)
-#include <string_view> // std::string_view (StringView)
-#include <stringzilla/stringzilla.hpp>
-#include <utility> // std::pair (Pair)
-#include <vector>  // std::vector (Vec)
+#include <array>                       // std::array (Array)
+#include <future>                      // std::future (Future)
+#include <map>                         // std::map (Map)
+#include <memory>                      // std::shared_ptr and std::unique_ptr (SharedPointer, UniquePointer)
+#include <mutex>                       // std::mutex and std::lock_guard (Mutex, LockGuard)
+#include <optional>                    // std::optional (Option)
+#include <string>                      // std::string (String, StringView)
+#include <string_view>                 // std::string_view (StringView)
+#include <stringzilla/stringzilla.hpp> // ashvardanian::stringzilla::{string, string_view}
+#include <utility>                     // std::pair (Pair)
+#include <vector>                      // std::vector (Vec)
 
 namespace util::types {
   using u8  = std::uint8_t;  ///< 8-bit unsigned integer.
